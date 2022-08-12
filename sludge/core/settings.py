6a6ae3420c1ddbdf7cf4c67c422f26d72eeff6c3
Kwemange Nyagrowa <80250781+jeff254-kedjango@users.noreply.github.com>
2022-08-12 21:48:28 +0300

    Update settings.py

--- sludge/core/settings.py
+++ sludge/core/settings.py
@@ -88,6 +88,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
+POSTGRES_DB = os.environ.get('POSTGRES_DB')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+POSTGRES_USER = os.environ.get('POSTGRES_USER')
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
 
 DATABASES = {
     "default": {
