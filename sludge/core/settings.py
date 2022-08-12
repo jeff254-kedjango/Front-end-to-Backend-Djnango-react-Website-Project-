0338e8e15fddf625507e719a355b8861678f57a6
Kwemange Nyagrowa <80250781+jeff254-kedjango@users.noreply.github.com>
2022-08-12 14:57:49 +0300

    Update settings.py

--- sludge/core/settings.py
+++ sludge/core/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": POSTGRES_DB,
+        "USER": POSTGRES_USER,
+        "PASSWORD": POSTGRES_PASSWORD,
+        "HOST": POSTGRES_HOST,
+        "PORT": POSTGRES_PORT,
     }
 }
 
