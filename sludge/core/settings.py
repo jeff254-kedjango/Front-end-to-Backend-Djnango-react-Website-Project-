3b24a218d519b93474820e78d5bcfa9d3ee13778
Kwemange Nyagrowa <80250781+jeff254-kedjango@users.noreply.github.com>
2022-08-13 23:37:38 +0300

    Update settings.py

--- sludge/core/settings.py
+++ sludge/core/settings.py
@@ -96,16 +96,11 @@ POSTGRES_USER = os.environ.get('POSTGRES_USER')
 POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
 POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": POSTGRES_DB,
-#         "USER": POSTGRES_USER,
-#         "PASSWORD": POSTGRES_PASSWORD,
-#         "HOST": POSTGRES_HOST,
-#         "PORT": POSTGRES_PORT,
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+    }
+}
 
 
 AUTH_PASSWORD_VALIDATORS = [
